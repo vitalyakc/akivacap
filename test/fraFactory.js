2a65bb39092d8827cc5a3b8cb1a13211dceaece6
Armen <yozhyn1@gmail.com>
2019-10-23 14:59:32 +0300

    fra factory tests

--- test/fraFactory.js
+++ test/fraFactory.js
@@ -1,6 +1,7 @@
 const Agreement = artifacts.require('AgreementMock');
 const FraFactory = artifacts.require('FraFactory');
 const Config = artifacts.require('Config');
+const ERC20Token = artifacts.require('SimpleErc20Token');
 const Reverter = require('./helpers/reverter');
 const {assertReverts} = require('./helpers/assertThrows')
 const BigNumber = require('bignumber.js');
@@ -18,6 +19,8 @@ contract('FraFactory', async (accounts) => {
   const NOBODY = accounts[3];
   const ETH_A = '0x4554482d41000000000000000000000000000000000000000000000000000000';
   const WRONG_COLLATERAL = '0x0000000000000000000000000000000000000000000000000000000000000000'
+  const SOME_COLLATERAL = '0x1234500000000000000000000000000000000000000000000000000000000000'
+  const ADDRESS_NULL = '0x0000000000000000000000000000000000000000';
 
   const toBN = (num) => {
     return new BigNumber(num);
@@ -42,21 +45,155 @@ contract('FraFactory', async (accounts) => {
       assert.equal(await fraFactory.agreementImpl.call(), agreement.address);
     })
 
-    it('should not be possible to set agreement implementation by contract not a owner', async () => {
+    it('should not be possible to set agreement implementation by not a contract owner', async () => {
       await assertReverts(fraFactory.setAgreementImpl(accounts[0], {from: NOBODY}));
       assert(await fraFactory.agreementImpl.call(), agreement.address);
     })
   })
 
   describe('initAgreementETH()', async () => {
-    it.only('should be possible to init agreement on ETH with valid values from borrower', async () => {
-      const result = await fraFactory.initAgreementETH(300000, 90000, 3, 
+    it('should be possible to init agreement on ETH with valid values from borrower', async () => {
+      await fraFactory.initAgreementETH(300000, 90000, 3, 
         ETH_A, {from: BORROWER, value: 2000});
 
-      const agreementProxyAddr = await fraFactory.initAgreementETH.call(300000, 90000, 3, 
+      assert.equal(await fraFactory.agreements.call(BORROWER, 0), 
+        await fraFactory.agreementList.call(0));
+      assert.notEqual(await fraFactory.agreements.call(BORROWER, 0), ADDRESS_NULL)
+
+      const localAgreement = await Agreement.at(await fraFactory.agreements.call(BORROWER, 0));
+
+      assert.equal(await localAgreement.borrower.call(), BORROWER);
+      assert.equal(await localAgreement.collateralAmount.call(), 2000);
+    })
+
+    it('should not be possible to init agreement on ETH with 0 transaction value from borrower', async () => {
+      await assertReverts(fraFactory.initAgreementETH(300000, 90000, 3, 
+        ETH_A, {from: BORROWER, value: 0}));
+     })
+  })
+
+  describe('initAgreementERC20()', async () => {
+    let erc20;
+    beforeEach('setup test erc20 token', async () => {
+      erc20 = await ERC20Token.new();
+      await agreement.setErc20Token(erc20.address);
+    })
+
+    // to fix in contract
+    it('should be possible to init agreement on erc20 token', async () => {
+      await erc20.mint(BORROWER, 2000);
+      await erc20.approve(fraFactory.address, 2000, {from: BORROWER});
+
+      console.log((await erc20.allowance(BORROWER, fraFactory.address)).toString());
+      console.log(await fraFactory.agreementImpl.call())
+      console.log(erc20.address)
+      console.log(await agreement.erc20TokenContract.call(ETH_A));
+      await fraFactory.initAgreementERC20(2000, 1000, 90000, 3, 
+        ETH_A, {from: BORROWER})
+
+      assert.equal(await fraFactory.agreements.call(BORROWER, 0), 
+      await fraFactory.agreementList.call(0));
+      assert.notEqual(await fraFactory.agreements.call(BORROWER, 0), ADDRESS_NULL)
+
+      const localAgreement = await Agreement.at(await fraFactory.agreements.call(BORROWER, 0));
+
+      assert.equal(await localAgreement.borrower.call(), BORROWER);
+      assert.equal(await localAgreement.collateralAmount.call(), 2000);
+    })
+
+    // to do
+    it('should be possible to initialize with bigger allovance', async () => {
+
+    })
+
+    // to do
+    it('should not be possible to initialize without allovance', async () => {
+
+    })
+
+    // to do
+    it('should not be possible to initialize with less allovance', async () => {
+
+    })
+  })
+
+  describe('approveAgreement()', async () => {
+    it('should be possible to approve agreement by owner', async () => {
+      await fraFactory.initAgreementETH(300000, 90000, 3, 
         ETH_A, {from: BORROWER, value: 2000});
 
-      assert.equal((await fraFactory.agreements.call(BORROWER, 0)), agreementProxyAddr);
+      const localAgreement = await Agreement.at(await fraFactory.agreements.call(BORROWER, 0));
+
+      assert.equal((await localAgreement.status.call()).toNumber(), 0);
+
+      await fraFactory.approveAgreement(localAgreement.address);
+
+      assert.equal((await localAgreement.status.call()).toNumber(), 1);
+    })
+
+    it('should not be possible to approve agreement by owner if status is not pending', async () => {
+      await fraFactory.initAgreementETH(300000, 90000, 3, 
+        ETH_A, {from: BORROWER, value: 2000});
+
+      const localAgreement = await Agreement.at(await fraFactory.agreements.call(BORROWER, 0));
+
+      assert.equal((await localAgreement.status.call()).toNumber(), 0);
+
+      await fraFactory.approveAgreement(localAgreement.address);
+
+      assert.equal((await localAgreement.status.call()).toNumber(), 1);
+
+      assert.isFalse(await fraFactory.approveAgreement.call(localAgreement.address));
+    })
+
+    it('should not be possible to approve agreement by not owner', async () => {
+      await fraFactory.initAgreementETH(300000, 90000, 3, 
+        ETH_A, {from: BORROWER, value: 2000});
+
+      const localAgreement = await Agreement.at(await fraFactory.agreements.call(BORROWER, 0));
+
+      assert.equal((await localAgreement.status.call()).toNumber(), 0);
+
+      await assertReverts(fraFactory.approveAgreement(localAgreement.address, {from: NOBODY}));
+
+      assert.equal((await localAgreement.status.call()).toNumber(), 0);
+
+    })
+
+    it('should not be possible to approve agreement by not owner if status is not pending', async () => {
+      await fraFactory.initAgreementETH(300000, 90000, 3, 
+        ETH_A, {from: BORROWER, value: 2000});
+
+      const localAgreement = await Agreement.at(await fraFactory.agreements.call(BORROWER, 0));
+
+      assert.equal((await localAgreement.status.call()).toNumber(), 0);
+
+      await fraFactory.approveAgreement(localAgreement.address);
+
+      assert.equal((await localAgreement.status.call()).toNumber(), 1);
+
+      await assertReverts(fraFactory.approveAgreement.call(localAgreement.address, {from:NOBODY}));
+    })
+
+    describe('batchApproveAgreements()', async () => {
+      it('should be possible to batch approve agreement by owner', async () => {
+        await fraFactory.initAgreementETH(300000, 90000, 3, 
+          ETH_A, {from: BORROWER, value: 2000});
+  
+        const localAgreement1 = await Agreement.at(await fraFactory.agreements.call(BORROWER, 0));
+  
+        await fraFactory.initAgreementETH(300000, 90000, 3, 
+          ETH_A, {from: BORROWER, value: 2000});
+  
+        const localAgreement2 = await Agreement.at(await fraFactory.agreements.call(BORROWER, 1));
+  
+        await fraFactory.initAgreementETH(300000, 90000, 3, 
+          ETH_A, {from: BORROWER, value: 2000});
+  
+        const localAgreement3 = await Agreement.at(await fraFactory.agreements.call(BORROWER, 2));
+
+        
+      })
     })
   })
 });
